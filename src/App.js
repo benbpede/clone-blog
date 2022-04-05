b558b47098a8e033a57baf2b48691b248167361c
benbpede <benbpede@gmail.com>
2022-04-05 20:20:52 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,42 @@ import React, { useState } from "react";
 import Modal from "./Modal";
 
 function App() {
-  let [글제목, 글제목변경] = useState([
-    "남자코트 추천",
-    "강남 우동맛집",
-    "파이썬 독학",
-  ]);
+  // let [글제목, 글제목변경] = useState([
+  //   "남자코트 추천",
+  //   "강남 우동맛집",
+  //   "파이썬 독학",
+  // ]);
   let [따봉, 따봉변경] = useState(0);
+
+  let [글제목, 글제목변경] = useState([
+    { id: 1, title: "남자코트 추천", count: 0 },
+    { id: 2, title: "강남 우동맛집", count: 0 },
+    { id: 3, title: "파이썬 독학", count: 0 },
+  ]);
 
   let [modal, modal변경] = useState(false);
 
+  var 배열 = [2, 3, 4];
+
+  var 곱하기배열 = 배열.map((arr) => arr * 2);
+
+  const 따봉카운트 = (i) => {
+    let copy = [...글제목];
+    copy[i].count++;
+    글제목변경(copy);
+  };
+  //개별 좋아요 고민중
   function 따봉더하기() {
     따봉변경(따봉 + 1);
   }
+
+  // function 반복된UI() {
+  //   var 어레이 = [];
+  //   for (var i = 0; i < 3; i++) {
+  //     어레이.push(<div>안녕</div>);
+  //   }
+  //   return 어레이;
+  // }
 
   // function 모달창열고닫기() {
   //   if (modal === false) {
@@ -40,7 +64,7 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <div className="list">
+      {/* <div className="list">
         <h3>
           {글제목[0]} <span onClick={따봉더하기}>👍</span>
           {따봉}
@@ -56,11 +80,33 @@ function App() {
       <div className="list">
         <h3>{글제목[2]}</h3>
         <p>2월 19일 발행</p>
+        <p>{곱하기배열}</p>
         <hr />
-      </div>
+      </div> */}
+
+      {글제목.map((배열, i) => (
+        <div className="list" key={배열.id}>
+          <h3>
+            {배열.title}
+            <span
+              onClick={(i) => {
+                let copy = [...글제목];
+                copy.count[i]++;
+                글제목변경(copy);
+              }}
+            >
+              👍
+            </span>
+            {배열.count}
+          </h3>
+          <p>2월 18일 발행</p>
+          <hr />
+        </div>
+      ))}
+
       <button onClick={모달창열고닫기}>버튼</button>
 
-      {modal === true ? <Modal /> : null}
+      {modal === true ? <Modal 글제목={글제목} /> : null}
     </div>
   );
 }
--- src/Modal.js
+++ src/Modal.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Modal = () => {
+const Modal = (props) => {
   return (
     <div className="modal">
-      <h2>제목</h2>
+      <h2>{props.글제목[0].title}</h2>
       <p>날씨</p>
       <p>상세내용</p>
     </div>
