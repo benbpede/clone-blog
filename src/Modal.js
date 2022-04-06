591ed1bf8385e790462756344d472a29c8b3e50f
benbpede <benbpede@gmail.com>
2022-04-06 20:45:34 +0900

    third commit

--- src/Modal.js
+++ src/Modal.js
@@ -3,7 +3,7 @@ import React from "react";
 const Modal = (props) => {
   return (
     <div className="modal">
-      <h2>{props.글제목[0].title}</h2>
+      <h2>{props.글제목[props.제목클릭].title}</h2>
       <p>날씨</p>
       <p>상세내용</p>
     </div>
